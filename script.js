2da29a0abab1972b9d3131e502d4e1cda291d9a4
Anh <nth.anh020209@gmail.com>
2023-02-27 14:59:32 +0700

    add html css js

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    // Ẩn modal khi tải trang
+    $('.modal').hide();
+    
+    // Xử lý sự kiện khi click vào nút Login
+    $('#login-btn').click(function() {
+        $('.modal').show();
+        $('.log-modal').show();
+        $('.res-modal').hide();
+    });
+    
+    // Xử lý sự kiện khi click vào nút Register
+    $('#register-btn').click(function() {
+        $('.modal').show();
+        $('.res-modal').show();
+        $('.log-modal').hide();
+    });
+    
+    // Xử lý sự kiện khi click vào nút đóng modal hoặc khoảng trống
+    $('.modal').click(function() {
+        $('.modal').hide();
+    });
+});
